cb0cd10f221fec20e1632fc5be962ea845adc61b
JoaoVictorDevMeta <joaovictorgouveiar@gmail.com>
2024-12-05 18:07:44 -0300

    adding push script for easier control

---
+++ git_commit_push.sh
@@ -0,0 +1,17 @@
+source .env
+
+# Stage all changes
+git add .
+
+# Ask for the commit message
+echo "Enter commit message: "
+read commit_message
+
+# Commit with the provided message
+git commit -m "$commit_message"
+
+github_username=$GITHUB_USERNAME
+github_token=$GITHUB_TOKEN
+
+# Push the changes
+git push https://$github_username:$github_password@github.com/JoaoVictorDevMeta/ListaTarefa-P1.git
